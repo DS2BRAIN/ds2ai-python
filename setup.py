0fc2e29d8542919a955aeb692dde30f5e09d4b43
yeo <ceo@dslab.global>
2022-07-15 18:04:18 +0900

    version 1.0.8 update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@
 
 from setuptools import setup
 setup(name="ds2ai",
-      version='1.0.7',
+      version='1.0.8',
       description="MLOps Platform - DS2.AI Python SDK",
       packages=["ds2ai"],
       author="DSLAB GLOBAL, INC",
